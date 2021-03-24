2b0fb3914909846f1f2140407eb1196e12ef54e6
Mohammad Shahzaib <shahzaibmohammad22@gmail.com>
2021-03-24 19:22:18 +0530

    last commit of the day

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import CategoryView from "./pages/CategoryView";
 import MovieView from "./pages/MovieView";
 
 function App() {
-
   const history = useHistory();
 
   useEffect(() => {
-    history.push('/discover/popular/125436345')
-  }, [])
+    history.push("/discover/popular/125436345");
+  }, []);
 
   return (
-    <Layout>
-      <Switch>
-        <Route exact path="/:gener/:name/:generId" component={CategoryView} />
-        <Route exact path="/:gener/:name/:generId/:movieId" component={MovieView} />
-      </Switch>
-    </Layout>
+    <>
+      <Layout>
+        <Switch>
+          <Route exact path="/:gener/:name/:generId" component={CategoryView} />
+          <Route
+            exact
+            path="/:gener/:name/:generId/:movieId"
+            component={MovieView}
+          />
+        </Switch>
+      </Layout>
+    </>
   );
 }
 
--- src/pages/MovieView.tsx
+++ src/pages/MovieView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "@material-ui/core";
 import { useParams } from "react-router";
 import axios from "axios";
@@ -10,13 +10,17 @@ type ParamTypes = {
   movieId: string;
 };
 
+
 function MovieView() {
   const { movieId } = useParams<ParamTypes>();
+
+  const [state, setState] = useState();
+
   // fetching only movie title, image path, and rating
   const fetchMovieDetails = async () => {
     const API_KEY = "6515b23812ca7dab83ed7195e34625d1";
 
-    const API_LINK = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`
+    const API_LINK = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`;
 
     try {
       const res = await axios(API_LINK);
@@ -24,7 +28,7 @@ function MovieView() {
       const data = res.data;
 
       let neededData = [];
-
+      setState(data);
       console.log(data);
     } catch (error) {}
   };
@@ -33,12 +37,24 @@ function MovieView() {
   }, []);
 
   return (
-    <Container>
-      <h1>I am movie view page {movieId}</h1>
-    </Container>
+    <section
+      style={{
+        width: "100%",
+        height: "100vh",
+        //  @ts-ignore
+        background: `url(http://image.tmdb.org/t/p/w1280${state?.backdrop_path})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+      }}
+    >
+      <Container>
+        <h1>I am movie view page </h1>
+      </Container>
+    </section>
   );
 }
 
 export default MovieView;
 
 // https://api.themoviedb.org/3/movie/527774?api_key=e366d974f73ae203397850eadc7bce1f&append_to_response=videos
+// http://image.tmdb.org/t/p/w1280/6zvLHD4rbHUNuRQLoIC2rkV8ayi.jpg
